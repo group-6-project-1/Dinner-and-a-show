87f73c2b9a1d6427ebef9f4abe7a90ec7492f7dc
Wendi Freeman <wfreeman2112@gmail.com>
2020-11-05 21:32:39 -0600

    initial AJAX call

---
+++ script.js
@@ -0,0 +1,17 @@
+//calling Eventbrite AJAX
+
+var settings = {
+"async" : true,
+"crossDomain": true,
+"url": "https://www.eventbriteapi.com/v3/events/49216045517/",
+"method": "GET",
+"headers": {
+        "Authorization": "Token X5QLAUR2EJUE5JM7JGSU",
+        "Content-Type": "application/json"
+        
+    }
+}
+
+$.ajax(settings).done(function (response) {
+    console.log(response)
+});

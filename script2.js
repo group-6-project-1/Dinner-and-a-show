48a6c01964e4fc754347476a4d2907ec019be110
Timih124 <tim.hackel24@gmail.com>
2020-11-13 21:33:01 -0600

    updating javascript to make it cleaner, added comment to be more indepth, updated title.

--- script2.js
+++ script2.js
@@ -1,12 +1,14 @@
 
 //Zomato api key
-//b9560497b1bfd6207f3bcdd820336297
+//Api key = b9560497b1bfd6207f3bcdd820336297
 
-// create zomato clas, and pull API with 
+// creating Zomato class, and using API key to pull data from source.
+// Making a template capture the Zomato APi which will end up showing on the HTML
+//Throwing Data & Function into an object. Zomato is the object, and pretty much creating a function
 class Zomato {
+	// declaring "function" by definition, but called constructor in class.
 	constructor() {
-
-		// adding in API to javascipt, new object for the page
+		// adding in API to javascipt, new object for the page. using Zomato API code. this will refernce the API.
 		this.api = "b9560497b1bfd6207f3bcdd820336297";
 		//pulling headers from API page to gather data from API.
 		this.header = {
@@ -15,34 +17,26 @@ class Zomato {
 				'user-key': this.api,
 				'Content-Type': 'application/json'
 			},
-			credentials: 'same-origin'
 		}
 	}
-
-
-	// search api for city and city to be able to return resturants
+	// search api for city (Zomato API) and city to be able to return resturants
+	// creating function searchAPI to handle promises
 	async searchAPI(city) {
 
-		const cityURL = `https://developers.zomato.com/api/v2.1/cities?q=${city}
-		`
-
+		const cityURL = `https://developers.zomato.com/api/v2.1/cities?q=${city}`
 		//search city
 		const cityInfo = await fetch(cityURL, this.header);
-			console.log(cityInfo);
+				//console.log(cityInfo);
 		const cityJSON = await cityInfo.json()
-
-					// goes through API to locate city suggestion based of search
+		// goes through API to locate city suggestion based of search
 		const cityLocation = await cityJSON.location_suggestions;
-		 
 		// loop to grab cityid with city search
 		let cityID = 0
 		if(cityLocation.length !== 0) {
 			cityID = await cityLocation[0].id
 		}
-
-		//search restaurant in zomato API to find results
-		const restaurantURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city
-		`
+		//search restaurant in zomato API to find results, constructing the Zomato URL for page restaurants
+		const restaurantURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city`
 		const restaurantInfo = await fetch(restaurantURL, this.header)
 		const restaurantJSON = await restaurantInfo.json()
 		const restaurants = await restaurantJSON.restaurants
@@ -55,12 +49,11 @@ class Zomato {
 		}
 	}
 }
+// create class TVmaze to pull in that API to represnt the TV show pulled from the city search and first resturant
 class Tvmaze{
-    constructor (){
-     
+    constructor (){  
 
     }
-
     searchAPI (restaurant){
 		console.log("restaurant cuisines",restaurant)
 		var show = restaurant.restaurant.cuisines.split(', ')[0]        
@@ -80,13 +73,11 @@ class Tvmaze{
         
     }
 }
-
-// creating the user interface for the entire div and feedback 
-class UI {
+// creating a class, object is the restaurants that will be displayed from searched cities
+class Restaurants {
 	constructor() {
 		this.restaurantList = document.querySelector('#restaurant-list')
 	}
-
 // creating results on if a city was found or if not found in the data base
 	showFeedback(text) {
 		const feedback = document.querySelector('.feedback');
@@ -97,20 +88,17 @@ class UI {
 		
 	}
 
-// get resturants from the api, llok up their menu, zomato site, address
-
+// get resturants from the api, look up their menu, zomato site, address
 	getRestaurants(restaurants) {
-
 		// results if no ciy exist exsist
 		if(restaurants.length === 0) {
 			this.showFeedback('no city/check spelling')
-
 			// if city does exist, insert the innerhtml doc that includes, img, name, address, menu_url
 		} else {
+			// loop to display four
 			 var i = 0 
 			this.restaurantList.innerHTML = '';
 			restaurants.forEach(restaurant => {
-
 				const { thumb:img, name, location:{address},menu_url,url } = restaurant.restaurant;
 
 				if(img !== '' && i++<4) {
@@ -119,70 +107,55 @@ class UI {
 			})
 		}
 	}
-
-
 	//display the resturant on the screen by creating a div to insert through the web api
 	showRestaurant(img, name, address, menu_url,url) {
 		const div = document.createElement('div');
 		div.setAttribute('class','col s3');
-// insert dive to the page where resturant page will be loaded
+				// insert dive to the page where resturant page will be loaded
 		div.innerHTML = `
-			
-			 
 					<img src="${img}" class="is-fullwidth" alt="">
-		 
-					<h6 class="text-uppercase pt-2 redText">${name}</h6>
+					<h6>${name}</h6>
 					<p>${address}</p>
-				 
-			 
-					<a href="${menu_url}" target="_blank" class="btn redBtn  text-uppercase"><i class="fas fa-book"></i>Menu</a>
-			 
-					<a href="${url}" target="_blank" class="btn redBtn  text-uppercase"><i class="fas fa-book"></i>Zomato Page</a>
-			 
-		 
-			 
+					<a href="${menu_url}" target="_blank" class="btn"><i class="fas fa-book"></i>Menu</a>
+					<a href="${url}" target="_blank" class="btn"><i class="fas fa-book"></i>Zomato Page</a>
 		`
 		this.restaurantList.appendChild(div)
 	}
 	
 }
-
-
 // function to return element on the page and feeback receieved if the no city exisit. 
 (function(){
 	const searchForm = document.getElementById('searchForm')
 	const searchCity = document.getElementById('searchCity')
 	const zomato = new Zomato()
-	const ui = new UI()
+	const newRest = new Restaurants()
 	const tv = new Tvmaze()
-
 	//using the submit button to gather city
-	searchForm.addEventListener('submit', e => {
-		e.preventDefault()
 
-// construct cityValue and also return city serached to lower case
-		const cityValue = searchCity.value.toLowerCase()
-
-		if(cityValue != '') {
-			//logic goes if populated value is true
-			zomato.searchAPI(cityValue)
-			.then(data => {
-				if(data.cityID !== 0) {
-					// console.log(data.cityID)
-					zomato.searchAPI(cityValue)
+	searchForm.addEventListener('submit', function (e) {
+			e.preventDefault();
+			// construct cityValue and also return city serached to lower case
+			const cityValue = searchCity.value.toLowerCase();
+			if (cityValue != '') {
+				//logic goes if populated value is true
+				zomato.searchAPI(cityValue)
 					.then(data => {
-						ui.getRestaurants(data.restaurants)
+						if (data.cityID !== 0) {
+							// console.log(data.cityID)
+							zomato.searchAPI(cityValue)
+								.then(data => {
+									newRest.getRestaurants(data.restaurants);
 
-						tv.searchAPI(data.restaurants[0])
-					})
-				} else {
-					ui.showFeedback('Please enter a valid city')
-				}
-			})
-		} else {
-			ui.showFeedback('please enter a city')
-		}
-	})
+									tv.searchAPI(data.restaurants[0]);
+								});
+						} else {
+							ui.showFeedback('Please enter a valid city');
+						}
+					});
+			} else {
+				ui.showFeedback('please enter a city');
+			}
+		})
 
 
 })()
